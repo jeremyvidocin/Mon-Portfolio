2ae2c29be8eca98467a114120ec264e628f101c4
VIDOCIN--BORDIN Jérémy <jeremyvidocin@gmail.com>
2025-04-05 13:43:50 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { ReactLenis } from 'lenis/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from '@gsap/react';
-import { Analytics } from "@vercel/analytics/react"
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
