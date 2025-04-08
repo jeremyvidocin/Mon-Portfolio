853b09f476807ab52bcf4671c48c439a14875f75
VIDOCIN--BORDIN Jérémy <jeremyvidocin@gmail.com>
2025-04-08 07:31:46 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () =>{
                         </div>
                     </div>
                     <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
-                    Bienvenu sur mon Portfolio
+                    Bienvenue sur mon Portfolio
                     </h2>
 
                     <div className="flex items-center gap-3">
